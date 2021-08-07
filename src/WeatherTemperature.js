69dde47b26a774b450d0fb404f8d029b9d89355a
silping <silviaping@gmail.com>
2021-08-07 21:30:43 +0100

    Updated the celsius units

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -5,7 +5,7 @@ export default function WeatherTemperature(props) {
   return (
     <div className="WeatherTemperature">
       <span className="temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">ºC | </span>
+      <span className="unit">ºC </span>
     </div>
   );
 }
